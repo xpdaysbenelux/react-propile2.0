e13b3282457094ff642c80eb0d83e815d7e1873b
Agnes De Pestel <agnes.depestel@icapps.com>
2020-02-17 16:36:21 +0100

    toevoeging Session handling bij layout en routes

--- src/_routing/layouts/unauthorized/UnauthorizedLayout.tsx
+++ src/_routing/layouts/unauthorized/UnauthorizedLayout.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import { Switch, Redirect, Route, NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Switch, Redirect, Route, NavLink, useRouteMatch, useLocation } from 'react-router-dom';
 import Auth from '../../../auth/Auth';
-import './unauthorizedLayout.scss';
-import { SvgLogo } from '../../../_assets/svg';
 import { translations } from '../../../_translations';
+import './unauthorizedLayout.scss';
+import logo from '../../../_assets/png/xpdaysLogo.png';
 
-export const UNAUTHORIZES_ROUTES = ['/auth'];
+export const UNAUTHORIZED_ROUTES = ['/auth'];
 
 const UnauthorizedLayout: React.FC = () => {
+  const { path } = useRouteMatch();
+  const { pathname } = useLocation();
+
+  const [canCreateSession, setSession] = useState(true);
+  useEffect(() => {
+    if (pathname === '/auth/create-session') {
+      setSession(false);
+    }
+  }, [pathname]);
+
   return (
     <div className="unauthorized-layout">
       <aside>
         <NavLink to="/">
-          <SvgLogo />
-          <span>{translations.getLabel('AUTH.SIDEBAR_TITLE')}</span>
+          <div onClick={() => setSession(true)}>
+            <img alt="Xpdays logo" src={logo} />
+            <span>{translations.getLabel('AUTH.SIDEBAR_TITLE')}</span>
+          </div>
         </NavLink>
+        {canCreateSession && (
+          <div onClick={() => setSession(false)}>
+            <NavLink to={`${path}/create-session`}>{translations.getLabel('SESSIONS.CREATE.TITLE')}</NavLink>
+          </div>
+        )}
       </aside>
       <Switch>
         <Route component={Auth} path="/auth" />
