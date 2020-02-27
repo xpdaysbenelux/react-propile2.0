6622ff07242c233ac5c2ab16051ff942551d9eb8
Agnes De Pestel <agnes.depestel@icapps.com>
2020-02-27 15:45:11 +0100

    showing session values in EditSessionForm

--- src/sessions/edit/EditSession.tsx
+++ src/sessions/edit/EditSession.tsx
@@ -6,33 +6,41 @@ import classnames from 'classnames';
 import { sessionsSelectors } from '../../_store/selectors';
 import { sessionsActions } from '../../_store/actions';
 import { translations } from '../../_translations';
-import { IUpdateSessionForm, SessionDuration, SessionExpierenceLevel, SessionState, SessionTopic, SessionType } from '../_models';
+import {
+  IUpdateSessionForm,
+  SessionDuration,
+  SessionExpierenceLevel,
+  SessionState,
+  SessionTopic,
+  SessionType,
+  ISession,
+} from '../_models';
 import UpdateSessionForm from './EditSessionForm';
 
-const initialForm: IUpdateSessionForm = {
-  description: '',
-  duration: SessionDuration.HalfHour,
-  emailFirstPresenter: '',
-  emailSecondPresenter: '',
-  expierenceLevel: SessionExpierenceLevel.Novice,
-  goal: '',
-  intendedAudience: [],
-  laptopsRequired: true,
-  materialDescription: '',
-  materialUrl: '',
-  maxParticipants: 50,
-  neededMaterials: '',
-  otherLimitations: '',
-  outline: '',
-  roomSetup: '',
-  sessionState: SessionState.Draft,
-  shortDescription: '',
-  subTitle: '',
-  title: '',
-  topic: SessionTopic.TechnologyAndTechnique,
-  type: SessionType.HandsOn,
-  xpFactor: 0,
-};
+const getInitialForm = (session: ISession): IUpdateSessionForm => ({
+  description: session?.description || '',
+  duration: session?.duration || SessionDuration.HalfHour,
+  emailFirstPresenter: session?.firstPresenter.email || '',
+  emailSecondPresenter: session?.secondPresenter?.email || '',
+  expierenceLevel: session?.expierenceLevel || SessionExpierenceLevel.Novice,
+  goal: session?.goal || '',
+  // intendedAudience: [],
+  laptopsRequired: session?.laptopsRequired || true,
+  materialDescription: session?.materialDescription || '',
+  materialUrl: session?.materialUrl || '',
+  maxParticipants: session?.maxParticipants || 50,
+  neededMaterials: session?.neededMaterials || '',
+  otherLimitations: session?.otherLimitations || '',
+  outline: session?.outline || '',
+  roomSetup: session?.roomSetup || '',
+  sessionState: session?.sessionState || SessionState.Draft,
+  shortDescription: session?.shortDescription || '',
+  subTitle: session?.subTitle || '',
+  title: session?.title || '',
+  topic: session?.topic || SessionTopic.TechnologyAndTechnique,
+  type: session?.type || SessionType.HandsOn,
+  xpFactor: session?.xpFactor || 0,
+});
 
 const EditSession: FC = () => {
   const { id } = useParams();
@@ -40,11 +48,12 @@ const EditSession: FC = () => {
   const isSubmitting = useSelector(sessionsSelectors.isUpdatingSessionLoading);
   const error = useSelector(sessionsSelectors.errorCrudSession);
 
-  // const checkFilledValues = (): void => {};
+  const session = useSelector(sessionsSelectors.session(id));
+  const initialForm = getInitialForm(session);
 
-  return (
+  return session ? (
     <Container as="main" className={classnames('left-container', 'edit-session')}>
-      <h1>{translations.getLabel('SESSIONS.EDIT.TITLE', { sessionTitle: 'TestSession' })}</h1>
+      <h1>{translations.getLabel('SESSIONS.EDIT.TITLE', { sessionTitle: session.title })}</h1>
       <UpdateSessionForm
         buttons={<Button href="/sessions">{translations.getLabel('SHARED.BUTTONS.CANCEL')}</Button>}
         error={error}
@@ -54,7 +63,7 @@ const EditSession: FC = () => {
         submitForm={(values: IUpdateSessionForm) => dispatch(new sessionsActions.UpdateSession({ sessionId: id, values }))}
       ></UpdateSessionForm>
     </Container>
-  );
+  ) : null;
 };
 
 export default EditSession;
