309240e798c8865e8874259a0a7f6db8382ea791
Agnes De Pestel <agnes.depestel@icapps.com>
2020-03-23 10:27:45 +0100

    changed button href

--- src/conferences/overview/ConferencesOverview.tsx
+++ src/conferences/overview/ConferencesOverview.tsx
@@ -10,7 +10,7 @@ const ConferencesOverview: FC = () => {
     <Container as="main">
       <header className="header">
         <h3>{translations.getLabel('CONFERENCES.OVERVIEW.CONFERENCES')}</h3>
-        <Button href="/programs/create-conference" isTextLink>
+        <Button href="/conferences/create-conference" isTextLink>
           <Icon name="SvgAdd" size={1.6} />
           {translations.getLabel('CONFERENCES.OVERVIEW.CREATE_CONFERENCE')}
         </Button>
