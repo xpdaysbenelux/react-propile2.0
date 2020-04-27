b29c75dde2c3687b9e6359da4a617dfbf0d47b20
Agnes De Pestel <agnes.depestel@icapps.com>
2020-04-27 10:53:01 +0200

    css and logic changes

--- src/programs/edit-planning/EditProgramPlanning.tsx
+++ src/programs/edit-planning/EditProgramPlanning.tsx
@@ -3,25 +3,30 @@ import { useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
-import LoadingSpinner from '../../_shared/loadingSpinner/LoadingSpinner';
-import { programsSelectors } from '../../_store/selectors';
+import { programsSelectors, conferencesSelectors } from '../../_store/selectors';
 import { translations } from '../../_translations';
-import { IProgram } from '../_models';
-
-// TODO type the return
-// const initialTable = (program: IProgram) => {};
+import { formatTime } from '../../_utils/timeHelpers';
+import { GoBackLink } from '../../_shared';
+import LoadingSpinner from '../../_shared/loadingSpinner/LoadingSpinner';
+import './editProgramPlanning.scss';
 
 const EditProgramPlanning: FC = () => {
   const { id } = useParams();
   const program = useSelector(programsSelectors.program(id));
-  console.log(program);
+  const conference = useSelector(conferencesSelectors.conference(program.conference.id));
 
   return program ? (
     <Container as="main">
+      <GoBackLink label={translations.getLabel('PROGRAMS.GO_BACK')} to={`/conferences/${conference.id}`} />
       <h1>{translations.getLabel('PROGRAMS.PLANNING.TITLE', { programTitle: program.title })}</h1>
-      <div>
-        <p>The program starts at ... and ends at ...</p>
-        <p>There are ... rooms</p>
+      <div className="program-info">
+        <p>
+          {translations.getLabel('PROGRAMS.PLANNING.PROGRAM_START_AND_END_TIME', {
+            programEndTime: formatTime(program.endTime),
+            programStartTime: formatTime(program.startTime),
+          })}
+        </p>
+        <p>{translations.getLabel('PROGRAMS.PLANNING.PROGRAM_ROOMS_AMOUNT', { programRoomAmount: conference.rooms.length })}</p>
         <Link to={`/programs/edit/${program.id}`}>{translations.getLabel('PROGRAMS.PLANNING.EDIT_PARAMS')}</Link>
       </div>
       <p>Program planning</p>
